c4355358acf854b7f65c406635ee21a3c68801b2
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-03-21 13:55:25 +0530

    added random data for trello app

---
+++ src/data/db.js
@@ -0,0 +1,111 @@
+const data = [
+    // Todo
+    {
+      id: 1,
+      status: "todo",
+      title: "Task 1",
+      color: "#FF6B6B",
+    },
+    {
+      id: 2,
+      status: "todo",
+      title: "Task 2",
+      color: "#FFA07A",
+    },
+    {
+      id: 3,
+      status: "todo",
+      title: "Task 3",
+      color: "#FF6347",
+    },
+    {
+      id: 4,
+      status: "todo",
+      title: "Task 4",
+      color: "#FF4500",
+    },
+    {
+      id: 5,
+      status: "todo",
+      title: "Task 5",
+      color: "#FF0000",
+    },
+    // In Progress
+    {
+      id: 6,
+      status: "inProgress",
+      title: "Task 6",
+      color: "#FFD700",
+    },
+    {
+      id: 7,
+      status: "inProgress",
+      title: "Task 7",
+      color: "#FFA500",
+    },
+    {
+      id: 8,
+      status: "inProgress",
+      title: "Task 8",
+      color: "#FF8C00",
+    },
+    {
+      id: 9,
+      status: "inProgress",
+      title: "Task 9",
+      color: "#FF6347",
+    },
+    // Review
+    {
+      id: 10,
+      status: "review",
+      title: "Task 10",
+      color: "#00FF7F",
+    },
+    {
+      id: 11,
+      status: "review",
+      title: "Task 11",
+      color: "#98FB98",
+    },
+    {
+      id: 12,
+      status: "review",
+      title: "Task 12",
+      color: "#2E8B57",
+    },
+    {
+      id: 13,
+      status: "review",
+      title: "Task 13",
+      color: "#008000",
+    },
+    {
+      id: 14,
+      status: "review",
+      title: "Task 14",
+      color: "#00FF00",
+    },
+    // Done
+    {
+      id: 15,
+      status: "done",
+      title: "Task 15",
+      color: "#4682B4",
+    },
+    {
+      id: 16,
+      status: "done",
+      title: "Task 16",
+      color: "#1E90FF", 
+    },
+    {
+      id: 17,
+      status: "done",
+      title: "Task 17",
+      color: "#0000FF",
+    },
+  ];
+  
+  export default data;
+  
